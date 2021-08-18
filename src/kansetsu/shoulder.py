53047a4ad9ded782fdb8ffebd9b7f4244307afa3
沢谷 邦夫 <devel@fathens.org>
2021-08-18 08:37:51 +0100

    Add sample code for `ShoulderJoint`

--- src/kansetsu/shoulder.py
+++ src/kansetsu/shoulder.py
@@ -35,3 +35,27 @@ class ShoulderJoint:
         self.current_angle_x += degree_x
         self.current_angle_y += degree_y
         self.__move_to()
+
+
+# サンプルコード
+if __name__ == "__main__":
+    import lib.i2c.pca9685
+
+    def _proc(pca_pwm: lib.i2c.pca9685.PWM):
+        sg90_x = sg90.SG90by180(pca_pwm, 0)
+        sg90_y = sg90.SG90by180(pca_pwm, 1)
+        shoulder = ShoulderJoint(sg90_x, sg90_y)
+        shoulder.set_angle(90, 90)
+
+        while True:
+            try:
+                print("x: ", end="")
+                degree_x = float(input())
+                print("y: ", end="")
+                degree_y = float(input())
+
+                shoulder.move(degree_x, degree_y)
+            except ValueError:
+                print("Invalid input.")
+
+    lib.i2c.pca9685.run_with(_proc)
